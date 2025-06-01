a4b7eb9afb4c49d0e7398ba5eb58aeba93178f17
JulianeMaran32 <julianemaran@gmail.com>
2025-06-01 18:18:12 -0300

    test: Adiciona teste do controller e context.

--- spring-6-di/src/test/java/com/juhmaran/spring6di/Spring6DiApplicationTests.java
+++ spring-6-di/src/test/java/com/juhmaran/spring6di/Spring6DiApplicationTests.java
@@ -1,13 +1,38 @@
 package com.juhmaran.spring6di;
 
+import com.juhmaran.spring6di.controllers.MyController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 
-@SpringBootTest
+@SpringBootTest // Diz ao JUnit para rodar este teste com o Spring Boot, criando um Contexto Spring
 class Spring6DiApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+  @Autowired // Pede ao Spring para injetar o Contexto da Aplicação
+  ApplicationContext applicationContext;
 
+  @Autowired // Pede ao Spring para injetar diretamente o objeto MyController
+  MyController myController;
+
+  @Test // Marca um metodo como um teste
+  void testAutowireOfController() {
+    // Usa o myController que foi injetado diretamente
+    System.out.println(myController.sayHello());
+  }
+
+  @Test // Marca outro metodo como um teste
+  void testGetControllerFromCtx() {
+    // Pede o bean MyController ao Contexto da Aplicação que foi injetado
+    MyController myController = applicationContext.getBean(MyController.class);
+    System.out.println(myController.sayHello());
+  }
+
+  @Test // Mais um metodo de teste (este não faz nada específico)
+  void contextLoads() {
+    // Este teste verifica se o contexto Spring carrega corretamente
+  }
 }
+
+
+
