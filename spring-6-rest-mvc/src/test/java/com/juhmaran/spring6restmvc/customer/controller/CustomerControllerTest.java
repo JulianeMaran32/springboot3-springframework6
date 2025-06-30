616ef8988cc2aaaee5c4148d8383d104a3a7f8f6
JulianeMaran32 <julianemaran@gmail.com>
2025-06-29 21:48:27 -0300

    test: [Customer Controller] [Tarefa 12] Assignment - Test Update Customer

--- spring-6-rest-mvc/src/test/java/com/juhmaran/spring6restmvc/customer/controller/CustomerControllerTest.java
+++ spring-6-rest-mvc/src/test/java/com/juhmaran/spring6restmvc/customer/controller/CustomerControllerTest.java
@@ -13,11 +13,13 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.UUID;
+
 import static org.hamcrest.core.Is.is;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(CustomerController.class)
@@ -37,6 +39,19 @@ class CustomerControllerTest {
   @BeforeEach
   void setUp() {
     customerServiceImpl = new CustomerServiceImpl();
+  }
+
+  @Test
+  @DisplayName("Update Customer")
+  void testUpdateCustomer() throws Exception {
+    Customer customer = customerServiceImpl.getAllCustomers().getFirst();
+    mockMvc.perform(put("/api/v1/customer/" + customer.getId())
+        .contentType(MediaType.APPLICATION_JSON)
+        .accept(MediaType.APPLICATION_JSON)
+        .contentType(MediaType.APPLICATION_JSON)
+        .content(objectMapper.writeValueAsString(customer)))
+      .andExpect(status().isNoContent());
+    verify(customerService).updateCustomerById(any(UUID.class), any(Customer.class));
   }
 
   @Test
