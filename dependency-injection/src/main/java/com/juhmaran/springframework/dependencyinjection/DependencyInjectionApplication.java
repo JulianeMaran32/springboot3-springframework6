8b399157c3bb0e7ca0a7c336944baf867ffa18b3
JulianeMaran32 <julianemaran@gmail.com>
2025-08-24 09:22:49 -0300

    The Spring Context

--- dependency-injection/src/main/java/com/juhmaran/springframework/dependencyinjection/DependencyInjectionApplication.java
+++ dependency-injection/src/main/java/com/juhmaran/springframework/dependencyinjection/DependencyInjectionApplication.java
@@ -1,13 +1,18 @@
 package com.juhmaran.springframework.dependencyinjection;
 
+import com.juhmaran.springframework.dependencyinjection.controllers.MyController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class DependencyInjectionApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DependencyInjectionApplication.class, args);
-	}
+  public static void main(String[] args) {
+    ApplicationContext ctx = SpringApplication.run(DependencyInjectionApplication.class, args);
+    MyController controller = ctx.getBean(MyController.class);
+    System.out.println("In Main Method");
+    System.out.println(controller.sayHello());
+  }
 
 }
---
+++ dependency-injection/src/main/resources/docs/Assignment.md
@@ -0,0 +1,27 @@
+# Assignment
+
+## 1. Create Spring DI Example Project
+
+### Instruções
+
+Create New Spring Project
+
+* Use Spring Initializr to Create New Spring Boot Project
+* Use Java 17, Spring Boot 3.x, Maven
+* Simple Project, No Dependencies Required
+* Download Project to Your Working Directory
+
+### Pergunta
+
+**Can you use Java 11 with Spring Framework 6?**
+
+**Minha Resposta:** No, you can't use Java 11 with Spring Framework 6. Spring Framework 6.0 and newer versions require a minimum of Java 17.
+**Resposta do Instrutor:** No, Java 17 or higher is required for Spring Framework 6.
+
+---
+
+## 2.
+
+### Instruções
+
+### Pergunta
