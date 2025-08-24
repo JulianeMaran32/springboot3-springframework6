1a024a56f3d1a772076fdf9c0a3e85364f0dacce
JulianeMaran32 <julianemaran@gmail.com>
2025-08-23 21:42:23 -0300

    feat(publisher): adiciona publisher

---
+++ guru/src/main/java/com/juhmaran/springframework/guru/domain/Publisher.java
@@ -0,0 +1,11 @@
+package com.juhmaran.springframework.guru.domain;
+
+/**
+ * Created by Juliane Maran
+ * @since 23/08/2025
+ */
+public class Publisher {
+
+    // TODO: Implementar funcionalidades da classe Publisher
+
+}
---
+++ guru/src/main/java/com/juhmaran/springframework/guru/repository/PublisherRepository.java
@@ -0,0 +1,8 @@
+package com.juhmaran.springframework.guru.repository;
+
+/**
+ * Created by Juliane Maran
+ * @since 23/08/2025
+ */
+public interface PublisherRepository {
+}
