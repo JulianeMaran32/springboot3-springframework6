96e2c429897b7b0795eddfb0efc0de457334e1f8
JulianeMaran32 <julianemaran@gmail.com>
2025-08-24 09:29:34 -0300

    test: Spring Test Context

--- dependency-injection/src/test/java/com/juhmaran/springframework/dependencyinjection/DependencyInjectionApplicationTests.java
+++ dependency-injection/src/test/java/com/juhmaran/springframework/dependencyinjection/DependencyInjectionApplicationTests.java
@@ -1,13 +1,33 @@
 package com.juhmaran.springframework.dependencyinjection;
 
+import com.juhmaran.springframework.dependencyinjection.controllers.MyController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootTest
 class DependencyInjectionApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+  @Autowired
+  ApplicationContext applicationContext;
+
+  @Autowired
+  MyController myController;
+
+  @Test
+  void testAutowireOfController() {
+    System.out.println(myController.sayHello());
+  }
+
+  @Test
+  void testGetControllerFromCtx() {
+    MyController myController1 = applicationContext.getBean(MyController.class);
+    System.out.println(myController1.sayHello());
+  }
+
+  @Test
+  void contextLoads() {
+  }
 
 }
