156d4d4cc1990955c9540934eebb3e6b6ac65e30
JulianeMaran32 <julianemaran@gmail.com>
2025-08-24 12:30:21 -0300

    usando qualifiers

---
+++ dependency-injection/src/main/java/com/juhmaran/springframework/dependencyinjection/services/GreetingServicePropertyInjected.java
@@ -0,0 +1,13 @@
+package com.juhmaran.springframework.dependencyinjection.services;
+
+import org.springframework.stereotype.Service;
+
+@Service("propertyGreetingService")
+public class GreetingServicePropertyInjected implements GreetingService {
+
+  @Override
+  public String sayGreeting() {
+    return "Friends don't let friends to property injection!";
+  }
+
+}
---
+++ dependency-injection/src/main/java/com/juhmaran/springframework/dependencyinjection/services/GreetingServiceSetterInjection.java
@@ -0,0 +1,13 @@
+package com.juhmaran.springframework.dependencyinjection.services;
+
+import org.springframework.stereotype.Service;
+
+@Service("setterGreetingBean")
+public class GreetingServiceSetterInjection implements GreetingService {
+
+  @Override
+  public String sayGreeting() {
+    return "Hey I'm Setting a Greeting!";
+  }
+
+}
