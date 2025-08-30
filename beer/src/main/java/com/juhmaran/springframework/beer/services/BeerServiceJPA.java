922e5b5133fdb755be1719c14c983be7660a3a34
JulianeMaran32 <julianemaran@gmail.com>
2025-08-29 21:29:12 -0300

    feat(jpa-service): JPA Get Operations

--- beer/src/main/java/com/juhmaran/springframework/beer/services/BeerServiceJPA.java
+++ beer/src/main/java/com/juhmaran/springframework/beer/services/BeerServiceJPA.java
@@ -23,12 +23,16 @@ public class BeerServiceJPA implements BeerService {
 
   @Override
   public List<BeerDTO> listBeers() {
-    return List.of();
+    return beerRepository.findAll()
+      .stream()
+      .map(beerMapper::beerToBeerDto)
+      .toList();
   }
 
   @Override
   public Optional<BeerDTO> getBeerById(UUID id) {
-    return Optional.empty();
+    return Optional.ofNullable(beerMapper.beerToBeerDto(beerRepository.findById(id)
+      .orElse(null)));
   }
 
   @Override
--- beer/src/main/java/com/juhmaran/springframework/beer/services/CustomerServiceJPA.java
+++ beer/src/main/java/com/juhmaran/springframework/beer/services/CustomerServiceJPA.java
@@ -23,12 +23,16 @@ public class CustomerServiceJPA implements CustomerService {
 
   @Override
   public Optional<CustomerDTO> getCustomerById(UUID uuid) {
-    return Optional.empty();
+    return Optional.ofNullable(customerMapper.customerToCustomerDto(customerRepository.findById(uuid)
+      .orElse(null)));
   }
 
   @Override
   public List<CustomerDTO> getAllCustomers() {
-    return List.of();
+    return customerRepository.findAll()
+      .stream()
+      .map(customerMapper::customerToCustomerDto)
+      .toList();
   }
 
   @Override
